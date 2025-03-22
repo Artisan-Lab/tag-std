1b27d39ba1baafd23862c702d60a2aa16a92c216
aisr <china.xuhui@gmail.com>
2025-03-22 21:51:18 +0800

    update

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,7 +1,7 @@
 #![feature(vec_into_raw_parts)]
-
-use contract::contract;
 #[allow(unused_variables)]
+use crate::contract::extract_contract;
+use contract;
 use std::slice;
 
 struct MyStruct {
@@ -13,15 +13,18 @@ impl MyStruct {
     fn from(p: *mut u8, l: usize) -> MyStruct {
         MyStruct { ptr: p, len: l }
     }
-    #[cfg_attr(feature = "contract", 
-        contract(!Null(self.ptr); 
-            Align(self.ptr, u8); 
+    ///contract(!Null(self.ptr); Align(self.ptr, u8); Allocated(self.ptr, u8, self.len, *); Init(self.ptr, u8, self.len, *); ValidInt(self.len*sizeof(u8), [0,isize::MAX]); Alias(self.ptr, *);
+    #[extract_contract]
+    /*
+    #[cfg_attr(feature = "contract",
+        contract(!Null(self.ptr);
+            Align(self.ptr, u8);
             Allocated(self.ptr, u8, self.len, *);
             Init(self.ptr, u8, self.len, *);
             ValidInt(self.len*sizeof(u8), [0,isize::MAX]);
             Alias(self.ptr, *);
         )
-    )]
+    )]*/
     unsafe fn get(&self) -> &mut [u8] {
         slice::from_raw_parts_mut(self.ptr, self.len)
     }
@@ -30,5 +33,6 @@ impl MyStruct {
 fn main() {
     let (p, l, _c) = Vec::new().into_raw_parts();
     let a = MyStruct::from(p, l);
-    println!("{:?}", unsafe {a.get()});
+    println!("{:?}", unsafe { a.get() });
+    MyStruct::get_contract();
 }
