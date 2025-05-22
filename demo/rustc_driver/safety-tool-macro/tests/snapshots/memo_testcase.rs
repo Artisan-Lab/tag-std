271b5d0b33e25019ed3c982dd7a131c279c7c778
zjp <jiping_zhou@foxmail.com>
2025-05-22 08:52:08 +0000

    tests: snapshots expanded memo_testcase

---
+++ demo/rustc_driver/safety-tool-macro/tests/snapshots/memo_testcase.rs
@@ -0,0 +1,26 @@
+#![feature(prelude_import)]
+#![allow(dead_code)]
+#[prelude_import]
+use std::prelude::rust_2024::*;
+#[macro_use]
+extern crate std;
+use safety_tool_macro::safety;
+/// This is a single line.
+fn single_line() {}
+/// Line 1.
+/// Line 2.
+fn multi_lines() {}
+/// Line 1.
+/// Line 2.
+fn multi_lines2() {}
+/// Line 1.
+/// Line 2.
+/// Line 3.
+fn multi_lines3() {}
+#[rustc_main]
+#[coverage(off)]
+#[doc(hidden)]
+pub fn main() -> () {
+    extern crate test;
+    test::test_main_static(&[])
+}
