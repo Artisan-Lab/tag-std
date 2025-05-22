1884e8fac0bf5786b47fd66a4a4ede900be58f4a
zjp <jiping_zhou@foxmail.com>
2025-05-22 08:52:08 +0000

    tests: snapshots expanded memo_testcase

--- demo/rustc_driver/safety-tool-macro/tests/memo.rs
+++ demo/rustc_driver/safety-tool-macro/tests/memo.rs
@@ -1,8 +1,10 @@
+use expect_test::expect_file;
 use std::process::Command;
 
 #[test]
 fn basic() {
-    let stdout = Command::new("cargo-expand").args(["--test", "memo_testcase"]).output().unwrap();
+    let stdout =
+        Command::new("cargo").args(["expand", "--test", "memo_testcase"]).output().unwrap();
     let expanded = std::str::from_utf8(&stdout.stdout).unwrap();
-    println!("{expanded}");
+    expect_file!["snapshots/memo_testcase.rs"].assert_eq(expanded);
 }
