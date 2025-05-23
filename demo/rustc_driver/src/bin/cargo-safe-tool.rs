c7220eca619f1530bfc150c80120bfea0aa7f63d
zjp <jiping_zhou@foxmail.com>
2025-05-23 06:33:00 +0000

    cargo-safe-tool: don't substitute -
    seems cargo fill this through stdin?
    
    also fixes CARGO_SAFE_TOOL var.

--- demo/rustc_driver/src/bin/cargo-safe-tool.rs
+++ demo/rustc_driver/src/bin/cargo-safe-tool.rs
@@ -3,20 +3,20 @@ use std::{env::var, process::Command};
 fn main() {
     // Search cargo-safe-tool and safe-tool CLI through environment variables,
     // or just use the name if absent.
-    let cargo_safe_tool = &*var("CARGO_SATE_TOOL").unwrap_or_else(|_| "cargo-safe-tool".to_owned());
+    let cargo_safe_tool = &*var("CARGO_SAFE_TOOL").unwrap_or_else(|_| "cargo-safe-tool".to_owned());
     let safe_tool = &*var("SAFE_TOOL").unwrap_or_else(|_| "safe-tool".to_owned());
 
-    let mut args = std::env::args().collect::<Vec<_>>();
+    let args = std::env::args().collect::<Vec<_>>();
 
     if args.len() == 2 && args[1].as_str() == "-vV" {
         // cargo invokes `rustc -vV` first
         run("rustc", &["-vV".to_owned()], &[]);
     } else if std::env::var("WRAPPER").as_deref() == Ok("1") {
         // then cargo invokes `rustc - --crate-name ___ --print=file-names`
-        if args[1] == "-" {
-            // `rustc -` is a substitute file name from stdin
-            args[1] = "src/lib.rs".to_owned();
-        }
+        // if args[1] == "-" {
+        //     // `rustc -` is a substitute file name from stdin
+        //     args[1] = "src/main.rs".to_owned();
+        // }
 
         run(safe_tool, &args[1..], &[]);
     } else {
