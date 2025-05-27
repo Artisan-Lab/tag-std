0feffd9517e0ef68bb40bdbcc4140d23408ec833
zjp <jiping_zhou@foxmail.com>
2025-05-27 02:12:09 +0000

    safety-tool-lib: abort for cargo expand error

--- demo/rustc_driver/safety-tool-lib/tests/snapshots.rs
+++ demo/rustc_driver/safety-tool-lib/tests/snapshots.rs
@@ -2,10 +2,14 @@ use expect_test::expect_file;
 use std::{path::Path, process::Command};
 
 fn snapshot(test: &str) {
-    let stdout = Command::new("cargo")
-        .args(["expand", "--test", test])
-        .output()
-        .unwrap_or_else(|err| panic!("Failed to run {test}:\n{err}"));
+    let err = |e: &dyn std::fmt::Display| panic!("Failed to run `cargo expand --test {test}:\n{e}");
+    let stdout =
+        Command::new("cargo").args(["expand", "--test", test]).output().unwrap_or_else(|e| err(&e));
+    if !stdout.status.success() {
+        let e = std::str::from_utf8(&stdout.stderr).unwrap();
+        err(&e);
+    }
+
     let expanded = std::str::from_utf8(&stdout.stdout).unwrap();
     let dir = Path::new("snapshots");
     let path = {
