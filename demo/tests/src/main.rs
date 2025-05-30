c100c6a88d4e95c9eebcc97f9383968552f1887f
aisr <china.xuhui@gmail.com>
2025-05-30 13:02:02 +0800

    update tests

--- demo/tests/src/main.rs
+++ demo/tests/src/main.rs
@@ -1,10 +1,20 @@
 #![feature(vec_into_raw_parts)]
-#![allow(unused_variables)]
+#![feature(stmt_expr_attributes)]
 
+use safety_tool_lib::safety;
 use demo::MyStruct;
 
 fn main() {
     let (p, l, _c) = Vec::new().into_raw_parts();
     let a = MyStruct::from(p, l);
-    println!("{:?}", unsafe { a.get() });
+    println!(
+        "{:?}",
+        #[safety::discharges(Init(self.ptr, u8, self.len), memo = "a.ptr originates from a local vector, so it is not null")]
+        #[safety::dischages(InBound(self.ptr, u8, self.len), memo = "samilar as Init")]
+        #[safety::dischages(ValidNum(self.len*sizeof(u8), [0,isize::MAX]), memo = "why?" )]
+        #[safety::dischages(Alias(self.ptr), memo = "The aliases of a.ptr are not used after this statement.")]
+        unsafe {
+            a.get()
+        }
+    );
 }
