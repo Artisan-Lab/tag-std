62a2247ddc32beeb4204bca7f5e19738366ff590
zjp <jiping_zhou@foxmail.com>
2025-05-30 03:59:19 +0000

    feat: #[safety::discharge(Memo(UserProperty))]
    This is a ghost implementation. Only to demonstrate the  #[discharge] syntax.

--- demo/tests/src/main.rs
+++ demo/tests/src/main.rs
@@ -1,10 +1,13 @@
+#![feature(proc_macro_hygiene)]
 #![feature(vec_into_raw_parts)]
 #![allow(unused_variables)]
 
 use demo::MyStruct;
+use safety_tool_lib::safety;
 
 fn main() {
     let (p, l, _c) = Vec::new().into_raw_parts();
+    #[safety::discharge(Memo(UserProperty))]
     let a = MyStruct::from(p, l);
     println!("{:?}", unsafe { a.get() });
 }
