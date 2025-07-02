a693f598b25ea6fd4a696b736b2a8d1db1687e4b
zjp <jiping_zhou@foxmail.com>
2025-07-02 12:40:22 +0000

    safety-tool-rfl: move crate-type arg to non --version branch

--- safety-tool/src/bin/safety-tool-rfl/run_safety_tool.rs
+++ safety-tool/src/bin/safety-tool-rfl/run_safety_tool.rs
@@ -7,21 +7,21 @@ use safety_tool::utils::{
 // RUSTC_BOOTSTRAP=1 safety-tool tests/snippets/safety_lib_basic.rs -L target/safety-tool/lib/
 // -Zcrate-attr="feature(register_tool)" -Zcrate-attr="register_tool(rapx)" --crate-type=lib
 pub fn run(mut args: Vec<String>) -> Result<()> {
-    if !args.iter().any(|arg| arg.starts_with("--crate-type")) {
-        // default to compile lib crate unless `CRATE_TYPE=bin` exists
-        let crate_type = if std::env::var("CRATE_TYPE").map(|s| s == "bin").unwrap_or(false) {
-            "--crate-type=bin"
-        } else {
-            "--crate-type=lib"
-        };
-        args.push(crate_type.to_owned());
-    }
-
     let bin_safety_tool = sysroot::bin_safety_tool();
     if matches!(&*args[0], "--version" | "-V" | "-Vv" | "-vV") {
         // linux will parse the version output
         execute(bin_safety_tool.as_str(), &args, vec![])?;
     } else {
+        if !args.iter().any(|arg| arg.starts_with("--crate-type")) {
+            // default to compile lib crate unless `CRATE_TYPE=bin` exists
+            let crate_type = if std::env::var("CRATE_TYPE").map(|s| s == "bin").unwrap_or(false) {
+                "--crate-type=bin"
+            } else {
+                "--crate-type=lib"
+            };
+            args.push(crate_type.to_owned());
+        }
+
         args.extend(extra_rustc_args());
         info!("args = {args:#?}");
         let vars = vec![("RUSTC_BOOTSTRAP", "1")];
