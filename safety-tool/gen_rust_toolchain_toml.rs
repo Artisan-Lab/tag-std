7baa653ad385ec22c98f0562a5e73caac9081723
zjp <jiping_zhou@foxmail.com>
2025-07-10 07:05:04 +0000

    gen_rust_toolchain_toml.rs: modify ra features setting

--- safety-tool/gen_rust_toolchain_toml.rs
+++ safety-tool/gen_rust_toolchain_toml.rs
@@ -1,27 +1,39 @@
 #!/usr/bin/env -S RUSTC_BOOTSTRAP=1 cargo -Zscript
+
+---cargo
+[package]
+edition = "2024"
+[dependencies]
+serde_json = "1"
+---
+use std::io::{Seek, Write};
+
 fn main() {
     // The first argument as a project name.
     // e.g. ./gen_rust_toolchain_toml.rs rfl
-    let proj = std::env::args().nth(1).expect("Choose a project to generate rust-toolchain.toml");
-    Toolchain::new(&proj).write_rust_toolchain_toml();
+    let first_arg =
+        std::env::args().nth(1).expect("Choose a project to generate rust-toolchain.toml");
+    let proj = Project::new(&first_arg);
+    proj.write_rust_toolchain_toml();
+    proj.set_rust_analyzer_cargo_features();
 }
 
-#[derive(Clone, Copy, Debug)]
-enum Toolchain {
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Project {
     Std,
     RustForLinux,
     Asterinas,
     Default,
 }
 
-impl Toolchain {
+impl Project {
     /// Specified by `std`, `rfl`, `asterinas`, or nothing.
     fn new(proj: &str) -> Self {
         match &*proj.to_lowercase() {
-            "std" | "verify-rust-std" => Toolchain::Std,
-            "rfl" | "rust-for-linux" => Toolchain::RustForLinux,
-            "asterinas" => Toolchain::Asterinas,
-            _ => Toolchain::Default,
+            "std" | "verify-rust-std" => Project::Std,
+            "rfl" | "rust-for-linux" => Project::RustForLinux,
+            "asterinas" => Project::Asterinas,
+            _ => Project::Default,
         }
     }
 
@@ -29,10 +41,10 @@ impl Toolchain {
     /// Must update the value once toolchain is updated from submodules.
     fn channel(self) -> &'static str {
         match self {
-            Toolchain::Std => "nightly-2025-06-02",
-            Toolchain::RustForLinux => "1.87",
-            Toolchain::Asterinas => todo!("Haven't supported Asterinas yet."),
-            Toolchain::Default => todo!("Haven't decided which toolchain as default to support."),
+            Project::Std => "nightly-2025-06-02",
+            Project::RustForLinux => "1.87",
+            Project::Asterinas => todo!("Haven't supported Asterinas yet."),
+            Project::Default => todo!("Haven't decided which toolchain as default to support."),
         }
     }
 
@@ -48,12 +60,64 @@ components = ["llvm-tools", "rustc-dev", "rust-src", "rust-analyzer", "rustfmt",
     }
 
     fn write_rust_toolchain_toml(self) {
-        use std::io::Write;
-
         const FILE: &str = "rust-toolchain.toml";
         let s = self.rust_toolchain_str();
 
         let mut file = std::fs::File::create(FILE).unwrap();
         file.write_all(s.as_bytes()).unwrap();
+    }
+
+    /// Returns a name in [features] of Cargo.toml.
+    fn as_feature_name(self) -> &'static str {
+        match self {
+            Project::Std => "std",
+            Project::RustForLinux => "rfl",
+            Project::Asterinas => "asterinas",
+            Project::Default => todo!(),
+        }
+    }
+
+    fn from_feature_name(s: &str) -> Option<Self> {
+        Some(match s {
+            "std" => Project::Std,
+            "rfl" => Project::RustForLinux,
+            "asterinas" => Project::Asterinas,
+            _ => return None,
+        })
+    }
+
+    fn set_rust_analyzer_cargo_features(self) {
+        const FILE: &str = "./.vscode/settings.json";
+        const KEY: &str = "rust-analyzer.cargo.features";
+
+        let ra_settins = {
+            let mut opts = std::fs::OpenOptions::new();
+            opts.read(true).write(true);
+            opts.open(FILE).unwrap()
+        };
+        let mut r#override = false;
+
+        let mut json: serde_json::Value = serde_json::from_reader(&ra_settins).unwrap();
+        let features = json[KEY].as_array_mut().unwrap();
+        for feat in features {
+            // Replace old project feature by current one.
+            let name = feat.as_str().unwrap();
+            if let Some(old) = Self::from_feature_name(name) {
+                if old != self {
+                    let new = self.as_feature_name();
+                    println!("[.vscode/settings.json] replace {KEY}: {name} => {new}");
+                    *feat = new.into();
+                    r#override = true;
+                    break;
+                }
+            }
+        }
+
+        if r#override {
+            // clear content and write new json
+            (&ra_settins).seek(std::io::SeekFrom::Start(0)).unwrap();
+            ra_settins.set_len(0).unwrap();
+            serde_json::to_writer_pretty(ra_settins, &json).unwrap();
+        }
     }
 }
