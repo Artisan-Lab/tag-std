561e0c8e702b009fb9e95aaed3c0223357b4cb5e
zjp <jiping_zhou@foxmail.com>
2025-07-26 09:22:25 +0000

    safety-parser: fix complex expression in arguments
    tricks are not using Punctuated which is eager to consume more tokens

--- safety-tool/safety-parser/src/safety/mod.rs
+++ safety-tool/safety-parser/src/safety/mod.rs
@@ -1,6 +1,5 @@
-use proc_macro2::{TokenStream, TokenTree};
 use syn::{
-    parse::{Parse, ParseStream, Parser},
+    parse::{Parse, ParseStream},
     punctuated::Punctuated,
     token::Paren,
     *,
@@ -36,97 +35,157 @@ impl Parse for SafetyAttrArgs {
     }
 }
 
+impl SafetyAttrArgs {
+    pub fn property_reason(&self) -> impl Iterator<Item = (&Property, Option<&LitStr>)> {
+        self.args.iter().flat_map(|arg| arg.tags.iter().map(|prop| (prop, arg.desc.as_ref())))
+    }
+}
+
 #[derive(Debug)]
 pub struct PropertiesAndReason {
-    pub properties: Punctuated<Property, Token![,]>,
-    pub desc: Option<Description>,
+    pub tags: Vec<Property>,
+    pub desc: Option<LitStr>,
 }
 
 impl Parse for PropertiesAndReason {
     fn parse(input: ParseStream) -> Result<Self> {
-        Ok(PropertiesAndReason {
-            properties: input.step(|cursor| {
-                let mut tokens = TokenStream::new();
-                let mut rest = *cursor;
-                while let Some((tt, next)) = rest.token_tree() {
-                    if let TokenTree::Punct(punct) = &tt {
-                        let ch = punct.as_char();
-                        if ch == ':' || ch == ';' {
-                            // reached at `: "reason"` or `;`
-                            return Ok((Punctuated::parse_terminated.parse2(tokens)?, rest));
-                        }
-                    }
-                    tokens.extend([tt]);
-                    rest = next;
-                }
-                Ok((Punctuated::parse_terminated.parse2(tokens)?, rest))
-            })?,
-            desc: if input.peek(Token![:]) { Some(input.parse()?) } else { None },
-        })
+        let mut tags = Vec::<Property>::new();
+        let mut desc = None;
+
+        while !input.cursor().eof() {
+            let tag: TagNameType = input.parse()?;
+            let sp = if input.peek(Paren) {
+                let content;
+                parenthesized!(content in input);
+                let args = Punctuated::parse_terminated(&content)?;
+                Property { tag, args }
+            } else {
+                Property { tag, args: Default::default() }
+            };
+            tags.push(sp);
+
+            if input.peek(Token![,]) {
+                // consume `,` in multiple tags
+                let _: Token![,] = input.parse()?;
+            }
+            if input.peek(Token![:]) {
+                let _: Token![:] = input.parse()?;
+                // `:` isn't in args, thus parse desc
+                desc = Some(input.parse()?);
+                break;
+            }
+            if input.peek(Token![;]) {
+                // new grouped SPs
+                break;
+            }
+        }
+        Ok(PropertiesAndReason { tags, desc })
     }
 }
 
 #[derive(Debug)]
 pub struct Property {
     /// `SP` or `type::SP`. Single `SP` means `precond::SP`.
-    pub path: Path,
+    pub tag: TagNameType,
     /// Args in `SP(args)` such as `arg1, arg2`.
-    pub args: Option<PropertyArgs>,
+    pub args: Punctuated<Expr, Token![,]>,
 }
 
 impl Parse for Property {
     fn parse(input: ParseStream) -> Result<Self> {
         Ok(Property {
-            path: input.parse()?,
-            args: if input.peek(Paren) { Some(input.parse()?) } else { None },
+            tag: input.parse()?,
+            args: {
+                if input.peek(Paren) {
+                    let content;
+                    _ = parenthesized!(content in input);
+                    Punctuated::parse_terminated(&content)?
+                } else {
+                    Punctuated::default()
+                }
+            },
         })
     }
 }
 
 #[derive(Debug)]
-pub struct PropertyArgs {
-    pub token: Paren,
-    pub args: Punctuated<Expr, Token![,]>,
+pub enum TagNameType {
+    /// Single ident SP, default to `precond` type.
+    SP(Ident),
+    /// Typed SP: `type.SP`
+    TypeSP { typ: Ident, dot: Token![.], sp: Ident },
 }
 
-impl Parse for PropertyArgs {
+impl Parse for TagNameType {
     fn parse(input: ParseStream) -> Result<Self> {
-        let content;
-        Ok(PropertyArgs {
-            token: parenthesized!(content in input),
-            args: Punctuated::parse_terminated(&content)?,
+        let ident: Ident = input.parse()?;
+        Ok(if input.peek(Token![.]) {
+            TagNameType::TypeSP { typ: ident, dot: input.parse()?, sp: input.parse()? }
+        } else {
+            TagNameType::SP(ident)
         })
     }
 }
 
-#[derive(Debug)]
-pub struct Description {
-    pub token: Token![:],
-    pub desc: LitStr,
-}
+impl TagNameType {
+    pub fn name(&self) -> String {
+        match self {
+            TagNameType::SP(sp) => sp,
+            TagNameType::TypeSP { sp, .. } => sp,
+        }
+        .to_string()
+    }
 
-impl Parse for Description {
-    fn parse(input: ParseStream) -> Result<Self> {
-        Ok(Description { token: input.parse()?, desc: input.parse()? })
+    pub const DEFAULT_TYPE: &str = "precond";
+
+    pub fn typ(&self) -> String {
+        match self {
+            TagNameType::SP(_) => Self::DEFAULT_TYPE.to_string(),
+            TagNameType::TypeSP { typ, .. } => typ.to_string(),
+        }
+    }
+
+    pub fn name_type(&self) -> [String; 2] {
+        match self {
+            TagNameType::SP(sp) => [sp.to_string(), Self::DEFAULT_TYPE.to_string()],
+            TagNameType::TypeSP { typ, sp, .. } => [sp.to_string(), typ.to_string()],
+        }
     }
 }
 
 #[test]
 fn parse_safety_attr() {
-    let _: SafetyAttr = parse_str("#[safety {}]").unwrap();
+    let parse_attr = |s: &str| parse_str::<SafetyAttr>(s);
 
-    let attr = "#[safety { SP }]";
-    let attr: SafetyAttr = parse_str(attr).unwrap();
-    let arg = attr.args.args.first().unwrap();
-    let sp = arg.properties.first().unwrap();
-    let ident = sp.path.get_ident().unwrap();
-    assert_eq!(ident, "SP");
+    // empty SP
+    let _: SafetyAttr = parse_attr("#[safety {}]").unwrap();
+
+    // simplest SP
+    let attr = parse_attr("#[safety { SP }]").unwrap();
+    let sp = attr.args.property_reason().next().unwrap().0;
+    assert_eq!(sp.tag.name(), "SP");
+    assert_eq!(sp.tag.name_type(), [TagNameType::DEFAULT_TYPE, "SP"]);
+
+    // SP with path prefix and arguments
+    let attr = parse_attr("#[safety { hazard.Alias(p, q) }]").unwrap();
+    let sp = attr.args.property_reason().next().unwrap().0;
+    assert_eq!(sp.tag.name_type(), ["hazard", "Alias"]);
+    let args: Vec<_> = sp
+        .args
+        .iter()
+        .map(|arg| {
+            if let Expr::Path(path) = arg {
+                path.path.get_ident().unwrap().to_string()
+            } else {
+                unreachable!()
+            }
+        })
+        .collect();
+    assert_eq!(args, ["p", "q"]);
 }
 
 #[test]
 fn parse_safety_args() {
-    let parse_args = |s: &str| parse_str::<SafetyAttrArgs>(s);
-
     // vanilla SP
     _ = parse_args("SP").unwrap();
     _ = parse_args("SP1, SP2").unwrap();
@@ -155,5 +214,19 @@ fn parse_safety_args() {
 
     // arguments with other SPs
     _ = parse_args(r#" SP1(a), SP2: "reason"; SP3 "#).unwrap();
-    let _ = parse_args(r#" SP(a, b): "reason"; SP1, SP2: "reason"; SP3, SP4 "#).unwrap();
+    _ = parse_args(r#" SP(a, b): "reason"; SP1, SP2: "reason"; SP3, SP4 "#).unwrap();
+}
+
+#[cfg(test)]
+fn parse_args(s: &str) -> syn::Result<SafetyAttrArgs> {
+    parse_str::<SafetyAttrArgs>(s)
+}
+
+#[test]
+fn parse_safety_complex_args() {
+    // SP path prefix
+    _ = parse_args(r#" hazard.Alias(p, q) "#).unwrap();
+
+    // complex expressions in arguments
+    _ = parse_args(r#" hazard.Alias(A {a: self.a}, a::b(c![])) : "" "#).unwrap();
 }
