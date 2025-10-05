2bab2f8934ca9586d56d2575b6e8e70ca6a3253b
zjp <jiping_zhou@foxmail.com>
2025-10-05 03:21:33 +0000

    test(basic): delegate SP of slice::from_raw_parts_mut

--- safety-tool/tests/basic/src/main.rs
+++ safety-tool/tests/basic/src/main.rs
@@ -9,14 +9,16 @@ use safety_macro::safety;
 
 fn main() {
     let (p, l, _c) = Vec::new().into_raw_parts();
+    dbg!(p, l);
     let a = MyStruct::from(p, l);
     #[safety {
-        Init: "This is from a valid Vec object.";
-        InBound: "This is from a valid Vec object.";
-        ValidNum: "self.len is valid.";
-        Alias: "p is no longer used.";
-        RustdocLinkToItem,
-        Alive
+        NonNull(p): "Vec::new generate a dangling pointer, but it's not null";
+        ValidPtr(p, u8, l): "l is zero in this case, and zero size access is always valid";
+        Init(p, u8, l): "no element yet, so no need to initialize anything";
+        Alive(p, l): "there is no real data, so this is met";
+        Alias(p): "p is no longer used other than in MyStruct";
+        Align(p, u8): "Vec makes an aligned pointer";
+        ValidNum(l, [0, isize::MAX]): "l is zero here, thus in range"
     }]
     let val = unsafe { a.get() };
     println!("{val:?}");
