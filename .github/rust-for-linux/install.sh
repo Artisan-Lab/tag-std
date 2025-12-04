7c30a5cdb93f1e796c809d44e67b87df439df00f
zjp <jiping_zhou@foxmail.com>
2025-12-04 02:54:39 +0000

    ci: use tag-rust-for-linux repo instead of the one from os-checker

--- .github/rust-for-linux/install.sh
+++ .github/rust-for-linux/install.sh
@@ -5,11 +5,11 @@
 set -exou pipefail
 
 # Linux version
-# LINUX_REPO=https://github.com/Artisan-Lab/tag-rust-for-linux.git
-# LINUX_BRANCH=rust-next
+LINUX_REPO=https://github.com/Artisan-Lab/tag-rust-for-linux.git
+LINUX_BRANCH=rust-next
 # === temporary for CI ===
-LINUX_REPO=https://github.com/os-checker/linux
-LINUX_BRANCH=tag-rust-for-linux
+# LINUX_REPO=https://github.com/os-checker/linux
+# LINUX_BRANCH=tag-rust-for-linux
 
 # Download Linux at a specific commit
 if [ ! -d "linux" ]; then
@@ -17,7 +17,7 @@ if [ ! -d "linux" ]; then
   git -C linux init
   git -C linux remote add origin ${LINUX_REPO}
   git -C linux fetch --depth 1 origin ${LINUX_BRANCH}
-  git -C linux checkout FETCH_HEAD # 78f4e73
+  git -C linux checkout FETCH_HEAD # b66755f
 else
   echo "linux source code has been downloaded"
 fi
@@ -26,8 +26,8 @@ fi
 # see https://mirrors.edge.kernel.org/pub/tools/llvm/rust/
 llvm=llvm-20.1.7-rust-1.87.0-$(uname -m)
 if [ ! -d "${llvm}" ]; then
-  wget https://mirrors.edge.kernel.org/pub/tools/llvm/rust/files/$llvm.tar.xz
-  tar -xvf $llvm.tar.xz
+  wget https://mirrors.edge.kernel.org/pub/tools/llvm/rust/files/"$llvm".tar.xz
+  tar -xvf "$llvm".tar.xz
 else
   echo "llvm and rust toolchain have been downloaded"
 fi
