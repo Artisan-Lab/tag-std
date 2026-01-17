70d181f904fb3f3994e5b4efd0e2f1e5494527ea
zjp <jiping_zhou@foxmail.com>
2026-01-17 16:29:46 +0000

    tag-asterinas: generate collected tags

--- safety-tool/collect_tags.sh
+++ safety-tool/collect_tags.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -exuo pipefail
 
+./gen_rust_toolchain_toml.rs asterinas
 cargo install --path . --locked --debug -Fasterinas
 
 export UPG_DIR=$PWD/target/upg
@@ -14,3 +15,11 @@ pushd ./tests/demo/
 cargo clean
 cargo safety-tool
 cp "$UPG_DIR" . -r
+popd
+
+pushd ../tag-asterinas/ostd/
+cargo clean
+cargo safety-tool --target x86_64-unknown-none
+popd
+
+cp "$UPG_DIR"/_tags/ostd.json assets/collect_tags/
